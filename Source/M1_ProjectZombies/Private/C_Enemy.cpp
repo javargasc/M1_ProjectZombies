a29f75b827365c16a7fdc21a06ea890d465ae366
Yoisus <jesus.a.vargas.calvi@gmail.com>
2022-05-01 15:19:56 -0300

    Base enemy

---
+++ Source/M1_ProjectZombies/Private/C_Enemy.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Enemy.h"
+
+// Sets default values
+AC_Enemy::AC_Enemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnSensor = CreateDefaultSubobject<UPawnSensingComponent>("Pawn Sensor");
+	PawnSensor->SetPeripheralVisionAngle(45.f);
+	PawnSensor->SightRadius = 1500.f;
+}
+
+// Called when the game starts or when spawned
+void AC_Enemy::BeginPlay()
+{
+	Super::BeginPlay();
+	PawnSensor->OnSeePawn.AddDynamic(this, &AC_Enemy::OnSeePawn);
+}
+
+// Called every frame
+void AC_Enemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AC_Enemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AC_Enemy::OnSeePawn(APawn* OtherPawn)
+{
+	FString message = TEXT("Saw Actor ") + OtherPawn->GetName();
+	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "OnSeePawn: " + message);
+
+	AAIController* AIController = Cast<AAIController>(GetController());
+
+	if (AIController)
+	{
+		AIController->MoveToLocation(OtherPawn->GetNavAgentLocation(), -1, false);
+	}
+}
---
+++ Source/M1_ProjectZombies/Public/C_Enemy.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Perception/PawnSensingComponent.h"
+#include "AIController.h"
+#include "C_Enemy.generated.h"
+
+UCLASS()
+class M1_PROJECTZOMBIES_API AC_Enemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AC_Enemy();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float lifeAmount = 100;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool isDead = false;
+
+	UFUNCTION()
+		void OnSeePawn(APawn* OtherPawn);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UPawnSensingComponent* PawnSensor;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
